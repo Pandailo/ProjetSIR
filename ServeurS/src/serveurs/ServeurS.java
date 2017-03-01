c99077839b5a07ff946636d7ef044ccdd054f3de
Zen0b1a <gillet.ann@gmail.com>
2017-03-01 14:05:46 +0100

    Séparation programme P/serveur S

---
+++ ServeurS/src/serveurs/ServeurS.java
@@ -0,0 +1,13 @@
+
+package serveurs;
+
+public class ServeurS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
