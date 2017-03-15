0133e9e3b92c5ef9cc1563d7a48596f903926468
Zen0b1a <gillet.ann@gmail.com>
2017-03-15 17:04:46 +0100

    CA MARCHE MAINTENANT TROLOLO
    \o/

--- ProgrammeP/src/projetsir/CAM.java
+++ ProgrammeP/src/projetsir/CAM.java
@@ -1,4 +1,5 @@
 
+
 package projetsir;
 
 import static java.lang.Math.*;
@@ -6,191 +7,229 @@ import java.util.*;
 
 public class CAM 
 {
-    private int nb_attributs; //Taille de la matrice
+    private int nb_attributs;
+    private int nb_requetes;
     private int nb_sites;
     private int[] ordre; //Ordre des attributs 
-    private int[][] mat_d; //Matrice distribution
-    private int[][] mat_ut; //Matrice d'utilisation
+    private int[][] matrice_distribution; //Matrice distribution
+    private int[][] matrice_utilisation; //Matrice d'utilisation
     private int nb_fragments; //Nombre de fragments demandés
+    private int[] meilleure_subdivision;
+    private int meilleure_val_subdivision;
     
-    public CAM(int nb_attributs, int nb_sites, int[] ordre, int[][] mat_d, int[][] mat_ut, int nb_fragments)
+    public CAM(/*int[] ordre, int[][] matrice_distribution, int[][] matrice_utilisation,*/ int nb_fragments)
     {
-        this.nb_attributs = nb_attributs;
-        this.nb_sites = nb_sites;
-        this.ordre = ordre;
-        this.mat_d = mat_d;
-        this.mat_ut = mat_ut;
+        this.remplissage2();
+        /*this.ordre = ordre;
+        this.matrice_distribution = matrice_distribution;
+        this.matrice_utilisation = matrice_utilisation;*/
+        this.nb_sites = this.matrice_distribution[0].length;
+        this.nb_attributs = this.matrice_utilisation[0].length;
+        this.nb_requetes = this.matrice_utilisation.length;
         this.nb_fragments = nb_fragments;
-        //this.remplissage();
+        this.meilleure_subdivision = new int[this.nb_fragments];
+        this.meilleure_val_subdivision = Integer.MIN_VALUE;
     }
     
-    /*private void remplissage()
+    private void remplissage2()
     {
+        ordre = new int[5];
+        ordre[0] = 2;
+	ordre[1] = 0;
+	ordre[2] = 3;
+	ordre[3] = 4;
+        ordre[4] = 1;
+        
+        matrice_distribution = new int[2][2];
+	matrice_distribution[0][0] = 15;
+	matrice_distribution[0][1] = 35;
+	
+	matrice_distribution[1][0] = 0;
+	matrice_distribution[1][1] = 70;
+        
+        matrice_utilisation = new int[2][5];
+	matrice_utilisation[0][0] = 1;
+	matrice_utilisation[0][1] = 1;
+	matrice_utilisation[0][2] = 1;
+	matrice_utilisation[0][3] = 0;
+        matrice_utilisation[0][4] = 0;
+	
+	matrice_utilisation[1][0] = 1;
+	matrice_utilisation[1][1] = 0;
+	matrice_utilisation[1][2] = 0;
+	matrice_utilisation[1][3] = 1;
+        matrice_utilisation[1][4] = 1;
+    }
+    
+    private void remplissage()
+    {
+        ordre = new int[4];
         ordre[0] = 0;
 	ordre[1] = 2;
 	ordre[2] = 1;
 	ordre[3] = 3;
 	
-	mat_d[0][0] = 15;
-	mat_d[0][1] = 20;
-	mat_d[0][2] = 10;
+        matrice_distribution = new int[4][3];
+	matrice_distribution[0][0] = 15;
+	matrice_distribution[0][1] = 20;
+	matrice_distribution[0][2] = 10;
 	
-	mat_d[1][0] = 5;
-	mat_d[1][1] = 0;
-	mat_d[1][2] = 0;
+	matrice_distribution[1][0] = 5;
+	matrice_distribution[1][1] = 0;
+	matrice_distribution[1][2] = 0;
 	
-	mat_d[2][0] = 25;
-	mat_d[2][1] = 25;
-	mat_d[2][2] = 25;
+	matrice_distribution[2][0] = 25;
+	matrice_distribution[2][1] = 25;
+	matrice_distribution[2][2] = 25;
 	
-	mat_d[3][0] = 3;
-	mat_d[3][1] = 0;
-	mat_d[3][2] = 0;
+	matrice_distribution[3][0] = 3;
+	matrice_distribution[3][1] = 0;
+	matrice_distribution[3][2] = 0;
 	
-	mat_ut[0][0] = 1;
-	mat_ut[0][1] = 0;
-	mat_ut[0][2] = 1;
-	mat_ut[0][3] = 0;
+        matrice_utilisation = new int[4][4];
+	matrice_utilisation[0][0] = 1;
+	matrice_utilisation[0][1] = 0;
+	matrice_utilisation[0][2] = 1;
+	matrice_utilisation[0][3] = 0;
 	
-	mat_ut[1][0] = 0;
-	mat_ut[1][1] = 1;
-	mat_ut[1][2] = 1;
-	mat_ut[1][3] = 0;
+	matrice_utilisation[1][0] = 0;
+	matrice_utilisation[1][1] = 1;
+	matrice_utilisation[1][2] = 1;
+	matrice_utilisation[1][3] = 0;
 	
-	mat_ut[2][0] = 0;
-	mat_ut[2][1] = 1;
-	mat_ut[2][2] = 0;
-	mat_ut[2][3] = 1;
+	matrice_utilisation[2][0] = 0;
+	matrice_utilisation[2][1] = 1;
+	matrice_utilisation[2][2] = 0;
+	matrice_utilisation[2][3] = 1;
 	
-	mat_ut[3][0] = 0;
-	mat_ut[3][1] = 0;
-	mat_ut[3][2] = 1;
-	mat_ut[3][3] = 1;
-    }*/
+	matrice_utilisation[3][0] = 0;
+	matrice_utilisation[3][1] = 0;
+	matrice_utilisation[3][2] = 1;
+	matrice_utilisation[3][3] = 1;
+    }
     
     //Savoir si une requête est concernée par les attributs
     private boolean is_concern(int req, List<Integer> attributs)
     {
-        boolean reponse = true;
-        for(int i=0; i<attributs.size(); i++)
-        {
-            if(this.mat_ut[req][attributs.get(i)]==0)
-            {
-                reponse = false;
-                i = this.nb_attributs;
-            }
-        }
-        return reponse;
+        for(int i=0; i<this.nb_attributs; i++)
+            if(this.matrice_utilisation[req][i]==1 && !attributs.contains(i))
+                return false;
+        return true;
     }
     
     //Calcul de z
-    private int calcul_z(List<Integer> attributs)
+    private int calcul_z(List<Integer> requetes)
     {
         int z = 0;
-        for(int i=0; i<this.nb_attributs; i++)
-        {
-            if(is_concern(i, attributs))
-            {
-                for(int j=0; j<this.nb_sites; j++)
-                    z += this.mat_d[i][j];
-            }
-        }
+        for(int i=0; i<requetes.size(); i++)
+            for(int j=0; j<this.nb_sites; j++)
+                z += this.matrice_distribution[requetes.get(i)][j];
         return z;
     }
     
     //Valeur d'un placement
-    private int val_subdivision(int[] subdivision) //A CHANGER
+    private int val_subdivision(int[] subdivision)
     {
-        int val_subdivision = 0;
+        int val_subdivision = 1;
         List<Integer> requetes_non_utilisees = new ArrayList<Integer>();
-        List<Integer> attributs_utilises = new ArrayList<Integer>();
-        for(int i=0; i<this.nb_attributs; i++)
-            attributs_non_utilises.add(i);
-        for(int i=0; i<subdivision.length; i++)
+        List<Integer> requetes_utilisees = new ArrayList<Integer>();
+        List<Integer> attributs_concernes = new ArrayList<Integer>();
+        int indice_debut = 0;
+        int indice_fin = 0;
+        //Initialisation des requêtes non utilisées
+        for(int i=0; i<this.nb_requetes; i++)
+            requetes_non_utilisees.add(i);
+        for(int i=0; i<this.nb_fragments; i++)
         {
-            attributs_utilises.clear();
-            for(int j=0; j<subdivision[i]; j++)
+            //System.out.println("Placement subdi "+subdivision[i]);
+            attributs_concernes.clear();
+            requetes_utilisees.clear();
+            //Récupération des attributs contenus dans la partie de la subdivision
+            if(i==0)
             {
-                for(int k=0; k<this.nb_attributs; k++)
-                if(this.ordre[i])
-                attributs_utilises.add();
-            }
-        }
-        return val_subdivision;
-        /*int[] TQ = new int[this.nb_attributs];
-        int[] BQ = new int[this.nb_attributs];
-        int[] OQ = new int[this.nb_attributs];
-
-        int[] attributsTQ = new int[this.nb_attributs];
-        int[] attributsBQ = new int[this.nb_attributs];
-
-        int CTQ = 0;
-        int CBQ = 0;
-        int COQ = 0;
-
-        //Initialisation
-        for(int i=0; i<this.nb_attributs; i++)
-        {
-            TQ[i] = 0;
-            BQ[i] = 0;
-            OQ[i] = 1;
-
-            if(i<subdivision)
-            {
-                attributsTQ[this.ordre[i]] = 1;
-                attributsBQ[this.ordre[i]] = 0;
+                indice_debut = 0;
+                indice_fin = subdivision[i];
             }
             else
             {
-                attributsTQ[this.ordre[i]] = 0;
-                attributsBQ[this.ordre[i]] = 1;
+                if(i==this.nb_fragments-1)
+                {
+                    indice_debut = subdivision[i-1];
+                    indice_fin = this.nb_attributs;
+                }
+                else
+                {
+                    indice_debut = subdivision[i-1];
+                    indice_fin = subdivision[i];
+                }
             }
+            for(int k=indice_debut; k<indice_fin; k++)
+                attributs_concernes.add(this.ordre[k]); 
+            //Récupération des requêtes concernées par la partie de la subdivision
+            for(int k=0; k<this.nb_requetes; k++)
+                if(is_concern(k, attributs_concernes))
+                {
+                    requetes_utilisees.add(k);
+                    requetes_non_utilisees.remove((Object)k);
+                }
+            val_subdivision *= calcul_z(requetes_utilisees);
         }
-
-        //Répartition des requêtes
-        for(int i=0; i<this.nb_attributs; i++)
-        {
-            if(this.is_concern(i, attributsTQ))
-            {
-                TQ[i] = 1;
-                OQ[i] = 0;
-            }
-            if(this.is_concern(i, attributsBQ))
-            {
-                BQ[i] = 1;
-                OQ[i] = 0;
-            }
-        }
-
-        //Calcul
-        for(int i=0; i<this.nb_attributs; i++)
-        {
-            for(int j=0; j<this.nb_sites; j++)
-            {
-                CTQ += TQ[i]*this.mat_d[i][j];
-                CBQ += BQ[i]*this.mat_d[i][j];
-                COQ += OQ[i]*this.mat_d[i][j];
-            }
-        }
-
-        return (CTQ*CBQ-(int)pow(COQ, 2));*/
+        val_subdivision -= pow(calcul_z(requetes_non_utilisees), this.nb_fragments);
+        return val_subdivision;
     }
     
     //Donne la meilleure valeur pour la fragmentation
-    private int[] meilleur_placement()
+    private boolean meilleur_placement()
     {
-        int meilleur = 1;
-        int val_meilleur = this.val_subdivision(1);
-        for(int i=1; i<this.nb_attributs; i++)
+        int[] meilleur = new int[this.nb_fragments-1];
+        int[] subdivision_courante = new int[this.nb_fragments-1];
+        for(int i=0; i<this.nb_fragments-1; i++)
         {
-            if(this.val_subdivision(i)>val_meilleur)
-            {
-                val_meilleur = this.val_subdivision(i);
-                meilleur = i;
-            }     
+            meilleur[i] = i+1;
+            subdivision_courante[i] = i+1;
         }
-        int[] resultat = {meilleur,val_meilleur};
-        return resultat;
+        int val_meilleur = Integer.MIN_VALUE;
+        int val_courante = val_meilleur;
+        int indice_a_changer = this.nb_fragments-2;
+        boolean continuer = true;
+        //Déplacement de la fragmentation 
+        while(continuer)
+        {
+            //Vérification de la valeur de la subdivision courante
+            val_courante = this.val_subdivision(subdivision_courante);
+            if(val_courante>val_meilleur)
+            {
+                val_meilleur = val_courante;
+                for(int i=0; i<this.nb_fragments-1; i++)
+                    meilleur[i] = subdivision_courante[i];
+            }
+            //Changement du placement de la subdivision
+            indice_a_changer = this.nb_fragments-2;
+            while(indice_a_changer>=0 && 
+                    subdivision_courante[indice_a_changer]>=this.nb_attributs-1-(this.nb_fragments-2-indice_a_changer))
+                indice_a_changer--;
+            if(indice_a_changer==-1)
+                continuer = false;
+            else
+            {
+                for(int i=indice_a_changer; i<this.nb_fragments-1; i++)
+                {
+                    if(i==indice_a_changer)
+                        subdivision_courante[i]++;
+                    else
+                        subdivision_courante[i] = subdivision_courante[i-1]+1;
+                }
+            }
+        }
+        //Récupération des résultats dans les variables concernées
+        if(val_meilleur>this.meilleure_val_subdivision)
+        {
+            this.meilleure_val_subdivision = val_meilleur;
+            for(int i=0; i<this.nb_fragments-1; i++)
+                this.meilleure_subdivision[i] = meilleur[i];
+            return true;
+        }
+        return false;
     }
     
     //Permute la matrice ordre sur la gauche
@@ -205,38 +244,30 @@ public class CAM
     //Teste toutes les permutations et retourne les fragments séparés de la meilleure manière
     public int[][] meilleure_fragmentation()
     {
-        int[][] fragmentation = new int[2][this.nb_attributs];
-        int[] meilleur_placement = this.meilleur_placement();
+        int[][] fragmentation = new int[this.nb_fragments][this.nb_attributs];
+        //Initialisation des fragments
+        for(int i=0; i<this.nb_fragments; i++)
+            for(int j=0; j<this.nb_attributs; j++)
+                fragmentation[i][j] = -1;
+        
+        int fragment = 0;
         for(int i=0; i<this.nb_attributs; i++)
         {
-            if(i<meilleur_placement[0])
-                fragmentation[0][i] = this.ordre[i];
-            else
-                fragmentation[1][i-meilleur_placement[0]] = this.ordre[i];
-        }
-        for(int j=0; j<this.nb_attributs; j++)
-        {
-            this.permutation();
-            if(this.meilleur_placement()[0]>meilleur_placement[0])
+            if(this.meilleur_placement())
             {
-                meilleur_placement = this.meilleur_placement();
-                for(int i=0; i<this.nb_attributs; i++)
+                for(int j=0; j<this.nb_fragments; j++)
+                    for(int k=0; k<this.nb_attributs; k++)
+                        fragmentation[j][k] = -1;
+                fragment = 0;
+                for(int j=0; j<this.nb_attributs; j++)
                 {
-                    if(i<meilleur_placement[0])
-                        fragmentation[0][i] = this.ordre[i];
-                    else
-                        fragmentation[1][i-meilleur_placement[0]] = this.ordre[i];
+                    if(fragment<this.nb_fragments-1 && j>=this.meilleure_subdivision[fragment])
+                        fragment++;
+                    fragmentation[fragment][this.ordre[j]] = 1;
                 }
             }
-        }
-        System.out.println("p "+meilleur_placement[0]+" v "+meilleur_placement[1]);
-        for(int i=0; i<this.nb_attributs; i++)
-        {
-            if(i>=meilleur_placement[0])
-                fragmentation[0][i] = -1;
-            else
-                fragmentation[1][i+meilleur_placement[0]-2] = -1;
-        }
+            this.permutation();
+        }    
         return fragmentation;
     }
     
@@ -245,15 +276,13 @@ public class CAM
         for(int i=0; i<this.nb_attributs; i++)
             System.out.println("Ordre "+i+" : "+this.ordre[i]);
         int[][] f = meilleure_fragmentation();
-        for(int i=0; i<this.nb_attributs; i++)
-            System.out.println("Ordre "+i+" : "+this.ordre[i]);
-        for(int i=0; i<this.nb_attributs; i++)
+        for(int i=0; i<this.nb_fragments; i++)
         {
-            System.out.println("F1 "+i+" : "+f[0][i]);
-        }
-        for(int i=0; i<this.nb_attributs; i++)
-        {
-            System.out.println("F2 "+i+" : "+f[1][i]);
+            for(int j=0; j<this.nb_attributs; j++)
+            {
+                if(f[i][j]==1)
+                    System.out.println("Attribut "+j+", fragment "+i);
+            }
         }
     }
 }
